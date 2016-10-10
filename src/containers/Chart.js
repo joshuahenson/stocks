54ff6f9b2cac87a018d006109ac80bfb5cf43551
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-10-10 15:04:55 -0700

    Testing socket.io

--- src/containers/Chart.js
+++ src/containers/Chart.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import { VictoryChart } from 'victory';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:8080'); // TODO: Update localhost
 
 class Chart extends Component {
+  componentDidMount() {
+    socket.on('news', (data) => {
+      console.log(data);
+      socket.emit('my other event', { my: 'data' });
+    });
+  }
   render() {
     return (
       <VictoryChart />
