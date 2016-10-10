20714b4c2d3b0b32c702befd191c5d6b1f44a432
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-10-10 14:40:53 -0700

    Beginning stages of client

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chart from '../containers/Chart';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <Chart />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/containers/Chart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { VictoryChart } from 'victory';
+
+class Chart extends Component {
+  render() {
+    return (
+      <VictoryChart />
+    );
+  }
+}
+
+export default Chart;
