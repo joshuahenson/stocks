24e2ab45bff777df0a7d840c0106580b5c0c802e
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-10-14 18:13:42 -0700

    Added initial results from db to client state

--- src/containers/Chart.js
+++ src/containers/Chart.js
@@ -5,9 +5,15 @@ import io from 'socket.io-client';
 const socket = io.connect('http://localhost:8080'); // TODO: Update localhost
 
 class Chart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: []
+    };
+  }
   componentDidMount() {
     socket.on('history', (history) => {
-      console.log(history);
+      this.setState({ history });
       socket.emit('my other event', { my: 'data' }); // testing dummy response
     });
   }
