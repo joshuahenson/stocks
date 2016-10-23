8c33e97a01b9b10cb47e55885ce136d7116deb4b
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-10-23 10:49:37 -0700

    Add ticker data via api call

--- server/controllers/historyController.js
+++ server/controllers/historyController.js
@@ -1,4 +1,29 @@
 const History = require('../models/history');
+const axios = require('axios');
+
+// helper function I plan on using to update existing symbols daily and get info for new symbols
+// TODO: fix startDate in URL
+const loadHistory = (symbol) => {
+  axios.get(`http://marketdata.websol.barchart.com/getHistory.json?key=${process.env.BARCHART_KEY}&symbol=${symbol}&type=daily&startDate=20161006&order=asc`)
+    .then((res) => {
+      History.update(
+        { symbol: symbol.toUpperCase() },
+        { $set: { days: res.data.results } },
+        { upsert: true },
+        (err) => {
+          if (err) {
+            console.error(err);
+          }
+        }
+      );
+    });
+};
+
+const addSymbol = (req, res) => {
+  const symbol = req.body.symbol;
+  loadHistory(symbol);
+  res.status(200).end();
+};
 
 const getHistory = (socket) => {
   History.find().exec((err, history) => {
@@ -12,4 +37,4 @@ const getHistory = (socket) => {
   });
 };
 
-module.exports = { getHistory };
+module.exports = { getHistory, addSymbol };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,8 @@
+const historyController = require('../controllers/historyController');
+
 module.exports = (app) => {
   app.get('/testing', (req, res) => {
     res.json('testing');
   });
+  app.post('/addSymbol', historyController.addSymbol);
 };
