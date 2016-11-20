14a0870dee219d8522fedb3d49e139cc180dad25
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-11-20 00:38:38 -0800

    Set tick values

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,12 +3,23 @@ import { VictoryChart, VictoryAxis, VictoryLine } from 'victory';
 
 // TODO: Selector to select different timeframes other than just one year?
 const Chart = ({ history, colors }) => {
+  let ticks = [];
+  if (history.length > 0) {
+    const len = history[0].days.length;
+    ticks = [
+      history[0].days[0].tradingDay,
+      history[0].days[Math.floor(len / 3)].tradingDay,
+      history[0].days[Math.floor(len * 2 / 3)].tradingDay, // eslint-disable-line no-mixed-operators
+      history[0].days[len - 1].tradingDay
+    ];
+  }
   return (
     <div>
       <VictoryChart>
         <VictoryAxis
           offsetY={50}
           fixLabelOverlap
+          tickValues={ticks}
         />
         <VictoryAxis
           tickFormat={x => (`${Math.floor(x * 100)}%`)}
